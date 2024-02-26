8e391e37c968ef3692cc1ef581a0a6156e14253e
Ryoma Ishida <ryomappi0910@gmail.com>
2024-02-27 00:51:37 +0900

    引数が2つか4つの場合にみ動くようにした。動作確認もできた。

--- README.md
+++ README.md
@@ -14,12 +14,14 @@ src: ソースコードを保存するディレクトリ。中には以下のフ
 # 使い方
 
 ```bash
-python src/raman.py <img_path> <spectrum_output_name> <csv_path> <processed_output_name>
+python src/raman.py <img_path> <spectrum_output_name> [<csv_path> <processed_output_name>]
 ```
 
-のようにして使います。各引数の説明は以下の通りです。
+のようにして使います。[]で囲まれた引数は、smoothingやdenoiseを行いたい場合のみに使います。
+
+各引数の説明は以下の通りです。
 
 - `<img_path>`: 入力画像のパス。例えば`test.jpg`など。
-- `<spectrum_output_name>`: スペクトルの数値データ (csvファイル) の出力ファイル名。例えば`test`など。勝手に`.csv`が付きます。
+- `<spectrum_output_name>`: スペクトルのグラフ (pngファイル) の出力ファイル名。例えば`test`など。勝手に`.png`が付きます。
 - `<csv_path>`: smoothingやdenoiseを行いたいデータ (csvファイル) のパス。例えば`test.csv`など。
 - `<processed_output_name>`: 処理後のスペクトルの数値データ (csvファイル) の出力ファイル名。例えば`test`など。勝手に`.csv`が付きます。
--- src/raman.py
+++ src/raman.py
@@ -2,17 +2,26 @@ import sys
 from RamanSpectrumClass import RamanSpectrum
 
 
-def run(img_path, spectrum_output_name, csv_path, processed_output_name) -> None:
+def run(img_path, spectrum_output_name, csv_path=None, processed_output_name=None) -> None:
     raman_spectrum = RamanSpectrum(img_path)
+    print("Successfully read the image")
     raman_spectrum.get_spectrum(spectrum_output_name)
+    print("Successfully saved the spectrum graph")
     raman_spectrum.save_raw_spectrum(spectrum_output_name)
-    raman_spectrum.outFigCSV(csv_path, processed_output_name)
+    print("Successfully saved the raw spectrum data")
+    if csv_path is not None and processed_output_name is not None:
+        raman_spectrum.outFigCSV(csv_path, processed_output_name)
+        print("Successfully saved the processed spectrum data and graph")
 
 if __name__ == '__main__':
     args = sys.argv
     # 例外処理
-    if len(args) != 5:
-        print("Usage: python src/raman.py <img_path> <spectrum_output_name> <csv_path> <processed_output_name>")
+    if len(args) != 3 and len(args) != 5:
+        print("Usage: python src/raman.py <img_path> <spectrum_output_name> [<csv_path> <processed_output_name>]")
         sys.exit(1)
+    
     # 実行
-    run(args[1], args[2], args[3], args[4])
+    if len(args) == 3:
+        run(args[1], args[2])
+    else:
+        run(args[1], args[2], args[3], args[4])
